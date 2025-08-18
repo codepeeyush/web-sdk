cc549e927c1fb0056fa5b904bf67a58c3445bd8e
Peeyush <peeyush.delta4infotech@gmail.com>
2025-08-18 12:49:18 +0530

    refactor: Simplify log-analyzer structure by removing unused components and consolidating imports, enhancing maintainability and clarity in the application

--- examples/log-analyzer/src/app/page.tsx
+++ examples/log-analyzer/src/app/page.tsx
@@ -1,16 +1,5 @@
 "use client"
-import CTA from "./(components)/CTA";
-import Hero from "./(components)/Hero";
-import Services from "./(components)/Services";
-import Testimonials from "./(components)/Testimonials";
-
+import Content from "./(dashboard)/Content";
 export default function Home() {
-    return (
-        <>
-            <Hero />
-            <Services />
-            <Testimonials />
-            <CTA />
-        </>
-    );
+    return <Content />;
 }
